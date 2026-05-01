ea54b2ad8f2318b147c7f8e710115d54dc5be0cf
billionaredonation <5fermatop@gmail.com>
2026-05-02 01:49:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { show } from './router.js';
 import { initRuntime, getState } from './state.js';
 
-import '../pages/welcome1/welcome1.js';
-import '../pages/welcome2/welcome2.js';
-import '../pages/welcome3/welcome3.js';
-import '../pages/home/home.js';
+import '../pages/welcome1/welcome1.js?v=101';
+import '../pages/welcome2/welcome2.js?v=101';
+import '../pages/welcome3/welcome3.js?v=101';
+import '../pages/home/home.js?v=101';
 
 function renderBootError(error) {
   console.error(error);
@@ -58,6 +58,3 @@ async function boot() {
 }
 
 boot();
-
-
-
